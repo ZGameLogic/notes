9c1f558152e8d175a3a8a78268b2041acbf41a3d
zabory <benshabowski@gmail.com>
2026-07-17 19:34:43 -0500

    Add token refresh functionality and cron job integration

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,14 @@
 import { configService } from "@/app/lib/ConfigService";
+import {Cron} from "croner";
+import {refreshExpiredTokens} from "@/app/lib/AuthenticationService";
 
 export async function register() {
   await configService.init();
 
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { databaseService } = await import("@/app/lib/DatabaseService");
+    const {databaseService} = await import("@/app/lib/DatabaseService");
     await databaseService.init();
   }
+
+  new Cron('0 */5 * * * *', refreshExpiredTokens);
 }
