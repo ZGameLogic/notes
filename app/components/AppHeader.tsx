465aa60d30a121a73b8be9696d65afc03ffc8279
Ben Shabowski <benshabowski@gmail.com>
2026-07-19 14:51:54 -0500

    Refactor AppHeader component layout and add search functionality

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -3,17 +3,17 @@
 import {
   AppBar,
   Avatar,
-  ButtonBase, FormControl,
-  IconButton, InputLabel,
+  ButtonBase, Divider, FormControl, Grid,
+  IconButton, InputAdornment, InputLabel,
   ListItemIcon,
   Menu,
-  MenuItem, Select,
+  MenuItem, OutlinedInput, Paper, Select,
   Stack,
   Toolbar,
   Typography
 } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import {AccountCircle, Logout} from "@mui/icons-material";
+import {Logout, Search} from "@mui/icons-material";
 import {useMemo, useState, type MouseEvent} from "react";
 import { useRouter } from 'next/navigation';
 import {useAuthData} from "@/app/components/auth/useAuthData";
@@ -46,59 +46,116 @@ export default function AppHeader({ redirect_url, client_id }: AppHeaderProps){
 
   return <AppBar position="static">
     <Toolbar>
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ mr: 2 }}
+      <Grid
+        container
+        direction="row"
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: '100%'
+        }}
       >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Dungeons and Dragons Notes
-      </Typography>
-      <FormControl sx={{ m: 1, width: '15%' }}>
-        <InputLabel id={'camp_id'}>Campaign</InputLabel>
-        <Select
-          variant={'outlined'}
-          labelId={'camp_id'}
-          value={selectedCampaignId ?? ''}
-          onChange={(e) => setSelectedCampaignId(e.target.value)}
-        >
-          {campaigns.map(camp => <MenuItem key={camp.id} value={camp.id}>{camp.name}</MenuItem>)}
-        </Select>
-      </FormControl>
-      {authData ? <>
-        <ButtonBase onClick={(event: MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget)}>
-          <Stack direction={'row'} spacing={1} sx={{alignItems: 'center'}}>
-            <Typography>{authData.username}</Typography>
-            <Avatar src={`https://cdn.discordapp.com/avatars/${authData.id}/${authData.avatar}.png?size=56`}/>
+        <Grid size={4}>
+          <Stack direction={'row'} sx={{alignItems: 'center'}}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Dungeons and Dragons Notes
+          </Typography>
           </Stack>
-        </ButtonBase>
-        <Menu
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
+        </Grid>
+        <Grid size={6}>
+          <Paper
+            variant="outlined"
+            sx={{
+              display: "flex",
+              alignItems: "stretch",
+              overflow: "hidden"
+            }}
+          >
+            <OutlinedInput
+              placeholder="Search"
+              endAdornment={
+                <InputAdornment position="end">
+                  <Search />
+                </InputAdornment>
+              }
+              sx={{
+                flex: 1,
+                "& fieldset": { border: "none" },
+              }}
+            />
+            <Divider orientation="vertical" flexItem />
+            <FormControl>
+              <InputLabel id={'camp_id'} />
+              <Select
+                variant="standard"
+                disableUnderline
+                value={selectedCampaignId ?? ""}
+                onChange={(e) => setSelectedCampaignId(e.target.value)}
+                sx={{
+                  px: 2,
+                  minWidth: 220,
+                }}
+              >
+                {campaigns.map(camp => <MenuItem key={camp.id} value={camp.id}>{camp.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+          </Paper>
+        </Grid>
+        <Grid
+          size={2}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
         >
-          <MenuItem onClick={handleLogout}>
-            <ListItemIcon>
-              <Logout fontSize="small" />
-            </ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
-      </> : <IconButton
-        size="large"
-        aria-label="account of current user"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
-        onClick={() => router.push(loginLink)}
-        color="inherit"
-      >
-        <AccountCircle />
-      </IconButton>
-      }
+          {authData ? <>
+            <ButtonBase
+              onClick={(event: MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget)}
+              sx={{ p: '1%' }}
+            >
+              <Stack direction={'row'} spacing={1} sx={{alignItems: 'center'}}>
+                <Typography>{authData.username}</Typography>
+                <Avatar
+                  sx={{ width: 24, height: 24 }}
+                  src={`https://cdn.discordapp.com/avatars/${authData.id}/${authData.avatar}.png?size=56`}
+                />
+              </Stack>
+            </ButtonBase>
+            <Menu
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleLogout}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
+          </> : <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={() => router.push(loginLink)}
+            color="inherit"
+          >
+            <Avatar sx={{ width: 24, height: 24 }} />
+          </IconButton>
+          }
+        </Grid>
+      </Grid>
     </Toolbar>
   </AppBar>;
 }
